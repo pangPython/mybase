13a234bf56458aff8d5e37bdb29ae5fc685bc855
pangPython <pangpython@qq.com>
2017-12-11 23:11:07 +0800

    查询数据库

---
+++ src/cn/pangpython/mybase/dbop/MyBatis.java
@@ -0,0 +1,30 @@
+package cn.pangpython.mybase.dbop;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyBatis {
+    public static void main(String[] args) throws IOException {
+        System.out.println("myBatis");
+
+        String resource = "cn/pangpython/mybase/dbop/mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+        User user=null;
+        try {
+            user = (User) session.selectOne("cn.pangpython.mybase.dbop.User.selectUser", 1);
+        } finally {
+            session.close();
+        }
+        System.out.println(user.getId());
+        System.out.println(user.getName());
+
+
+    }
+}
---
+++ test.sql
@@ -0,0 +1,31 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50553
+Source Host           : localhost:3306
+Source Database       : test
+
+Target Server Type    : MYSQL
+Target Server Version : 50553
+File Encoding         : 65001
+
+Date: 2017-12-11 23:10:39
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'hello');
